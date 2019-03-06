0266005d56731424c804e45dfd3baa0ea47d8e23
Foris <foris4@gmail.com>
2019-03-06 07:16:51 +0000

    new method read() and to do list of methods

--- SQL/SQL_IO.py
+++ SQL/SQL_IO.py
@@ -4,6 +4,7 @@ import os
 class Database():
 	def __init__(self):
 		self.tables_dict = {}
+		self.load()
 	
 	
 	def load(self, database="timer.db", file_path=os.getcwd()+"\\"):
@@ -47,13 +48,45 @@ class Database():
 		con = sqlite3.connect(file_path+database)
 		con.row_factory=sqlite3.Row
 		cur = con.cursor()
+		
+		
+		
 	
 		
+	def add_entry(self, id, start, end, **kwargs):
+		"""will add entry into tables_dict
+		unique id, use check_id method or auto_id method 
+		start need to be datetime object 
+		end need to be datetime object
+		kwargs are dict with tags where {tag name: 0 or 1}
+		"""
+		pass
 
+	def read(self):
+		"""return table_dict as new not connected object
+		"""
+		temp = {}
+		for key, value in self.tables_dict.items():
+			temp[key]=value
+		
+		return temp
 
-
-
-
+	def check_id(self, id):
+		"""will return true if id is in use
+		"""
+		pass
+		
+	def auto_id(self):
+		"""return usable, unique id for tables_dict
+		"""
+		pass
+		
+	
+	
+	
+	
+	
+	
 
 
 
