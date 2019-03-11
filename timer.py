9e756c79e1fe2c0aa5910de62294f785425ddf88
Foris <foris4@gmail.com>
2019-03-11 12:44:22 +0000

    new class Timer

---
+++ timer.py
@@ -0,0 +1,31 @@
+import datetime
+
+
+
+class Timer():
+	"""class with purpose to use for measure time
+	"""
+	def __init__(self):
+		self.data = {"timer":[]}
+		self.time = []
+		
+	
+	
+	def start(self):
+		"""start time
+		"""
+		pass
+		#self.time.append()
+		
+		
+	def end(self):
+		"""end time
+		"""
+		pass
+	
+	def set_data():
+		"""set all need data into usefull schema
+		"""
+	
+	
+	
