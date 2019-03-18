b88c167cf41e74113dff79c124226d2561480a17
Foris <foris4@gmail.com>
2019-03-18 19:38:04 +0000

    Cosmetic changes to code.

--- SQL/SQL_IO.py
+++ SQL/SQL_IO.py
@@ -2,181 +2,165 @@ import sqlite3
 import os
 
 
-class Database():
-	"""
-	"""
-	def __init__(self):
-		self.tables_dict = {}
-		self.load()
-		self.new_entry = {}
-		self.delete_entry = {}
-	"""
-	def __repr__(self):
-		return str(self.tables_dict)
-	"""
-	
-	def load(self, database="timer.db", file_path=os.getcwd()+"\\"):
-		"""This method will load database into two dictionaries
-		database need to be string with file name
-		file_path need to be string with path to database file
-		return all tables from database as {table:[list of row as dict]} into variable table_dict"""
-		self.tables_dict = {}
-		con=sqlite3.connect(file_path+database)
-		con.row_factory = sqlite3.Row
-		cur=con.cursor()
-	
-	
-		#get all table names from database
-		cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
-		tables = []
-		for table in cur.fetchall():
-			tables.append(table[0])
-		tables = tuple(tables)
-	
-	
-		#load all tables into {table:[list of row as dict]} and save to table_dict
-		for tab_ind in range(len(tables)):
-			cur.execute("SELECT * FROM {}".format(tables[tab_ind]))
-			table_list = []
-			timers = cur.fetchall()
-			for timer in timers:
-				temp={}
-				for index in range(len(timer)):
-					temp[timer.keys()[index]]=timer[index]
-				table_list.append(temp)
-			self.tables_dict[tables[tab_ind]]= table_list
-	
-	
-		con.close()
-	
-	
-	
-	def save(self, database="timer.db", file_path=os.getcwd()+"\\"):
-		"""This method will save changes in database into file
-		"""
-		con = sqlite3.connect(file_path+database)
-		con.row_factory=sqlite3.Row
-		cur = con.cursor()
-		
-		if self.new_entry:
-			for key, value in self.new_entry.items():
-				table = key
-				for item in value:
-					keys = []
-					values = []
-					for k in item.keys():
-						keys.append(k)
-						values.append(item[k])
-					keys = tuple(keys)
-					values = tuple(values)
-					
+class Database:
+    """
+    """
 
-					cur.execute("INSERT INTO {table} {keys} VALUES {values}".format(table = key, keys = keys, values = values))
-			self.new_entry = {}
-		
-		if self.delete_entry:
-			pass
-		
-		
-		con.commit()
-		con.close()
-		
-	def add_entry(self, dict):
-		"""will add entry into new_entry.
-		dict need to be send as {table_name:[list of new entry as dict]}
-		
-		plan to do: if table name or key in dict don't match names and keys in tables_dict then it will raise exception.
-					add posibility of add only part of column, for empty none/null
-		"""
-		for key, value in dict.items():
-			if key in self.tables_dict:
-				self.new_entry[key]=[]
-				for item in value:
-					if self.tables_dict[key]:
-						if item.keys()==self.tables_dict[key][0].keys() or not self.tables_dict[key][0]:
-							if self.check_id(item["id"]):
-								#self.tables_dict[key].append(value)
-								self.new_entry[key].append(item)
-								print("Entry: ", item)
-							else:
-								item["id"] = self.auto_id()
-								print("Id was used by other row, new unique id was added. New id is: {}\nWhole entry: {}".format(item["id"], item))
-								self.new_entry[key].append(item)
-						else:
-							print("Column names are not the same as they are in table.")
-					else:
-						self.new_entry[key].append(item)
-						print("Entry: ", item)
-						
-			else:
-				print("Table name is incorrect")
-		
-		
-		self.save()
-		self.load()
+    def __init__(self):
+        self.tables_dict = {}
+        self.load()
+        self.new_entry = {}
+        self.delete_entry = {}
 
-	def read(self):
-		"""return table_dict as nice looking string
-		"""	 
-		string = ""
-		for key, value in self.tables_dict.items():
-			string+="Table: "+key+"\n"
-			for item in value:
-				string+=str(item)+"\n"
-		return string
+    """
+    def __repr__(self):
+        return str(self.tables_dict)
+    """
 
-	def check_id(self, id, id_name="id"):
-		"""will return true if id is NOT in use
-		"""
-		temp_ids = []
-		for key, value in self.tables_dict.items():
-			for item in value:
-				temp_ids.append(item[id_name])
-		if id not in temp_ids:
-			return True
-		else:
-			return False
-		
-	def auto_id(self):
-		"""return usable, unique id for tables_dict
-		"""
-		id = 0
-		while not self.check_id(id):
-			id+=1
-		return id
-		
-	
-	def delete_entry(self, table, id):
-		"""will mark entry for delete
-		"""
-		pass
-	
-	
-	def add_colunm(self, table, column_name, data_type):
-		"""It will mark, that you want add new column into table. To save use save method! 
-		To avoid errors use this method separetly from other methods and save imedietly.
-		data_type integer, string/text, none/null, real/float, blob.
-		"""
-		pass
-	
-	def update_table(self, table, dict):
-		"""
-		dict need to be dict with id key and column_name key that you want edit
-		"""
+    def load(self, database="timer.db", file_path=os.getcwd() + "\\"):
+        """This method will load database into two dictionaries
+        database need to be string with file name
+        file_path need to be string with path to database file
+        return all tables from database as {table:[list of row as dict]} into
+        variable table_dict"""
+        self.tables_dict = {}
+        con = sqlite3.connect(file_path + database)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
 
+        # get all table names from database
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        tables = []
+        for table in cur.fetchall():
+            tables.append(table[0])
+        tables = tuple(tables)
 
+        # load tables into {table:[list of row as dict]} and save to table_dict
+        for tab_ind in range(len(tables)):
+            cur.execute("SELECT * FROM {}".format(tables[tab_ind]))
+            table_list = []
+            timers = cur.fetchall()
+            for timer in timers:
+                temp = {}
+                for index in range(len(timer)):
+                    temp[timer.keys()[index]] = timer[index]
+                table_list.append(temp)
+            self.tables_dict[tables[tab_ind]] = table_list
 
+        con.close()
 
+    def save(self, database="timer.db", file_path=os.getcwd() + "\\"):
+        """This method will save changes in database into file
+        """
+        con = sqlite3.connect(file_path + database)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
 
+        if self.new_entry:
+            for key, value in self.new_entry.items():
+                for item in value:
+                    keys = []
+                    values = []
+                    for k in item.keys():
+                        keys.append(k)
+                        values.append(item[k])
+                    keys = tuple(keys)
+                    values = tuple(values)
 
+                    cur.execute(
+                        "INSERT INTO {table} {keys} VALUES {values}".format
+                        (table=key, keys=keys, values=values))
+            self.new_entry = {}
 
+        if self.delete_entry:
+            pass
 
+        con.commit()
+        con.close()
 
+    def add_entry(self, dictionary):
+        """will add entry into new_entry.
+        dict need to be send as {table_name:[list of new entry as dict]}
 
+        plan to do: if table name or key in dict don't match names and keys in
+        tables_dict then it will raise exception.
+                    add possibility of add only part of column,
+                    for empty none/null
+        """
+        for key, value in dictionary.items():
+            if key in self.tables_dict:
+                self.new_entry[key] = []
+                for item in value:
+                    if self.tables_dict[key]:
+                        if item.keys() == self.tables_dict[key][0].keys() \
+                                or not self.tables_dict[key][0]:
+                            if self.check_id(item["id"]):
+                                # self.tables_dict[key].append(value)
+                                self.new_entry[key].append(item)
+                                print("Entry: ", item)
+                            else:
+                                item["id"] = self.auto_id()
+                                print(
+                                    "Id was used by other row, new unique id" 
+                                    "was added. New id is: {}\nWhole "  
+                                    "entry: {}".format(item["id"], item))
+                                self.new_entry[key].append(item)
+                        else:
+                            print("Column names are not the same as they are" 
+                                  "in table.")
+                    else:
+                        self.new_entry[key].append(item)
+                        print("Entry: ", item)
 
+            else:
+                print("Table name is incorrect")
 
+        self.save()
+        self.load()
 
+    def read(self):
+        """return table_dict as nice looking string
+        """
+        string = ""
+        for key, value in self.tables_dict.items():
+            string += "Table: " + key + "\n"
+            for item in value:
+                string += str(item) + "\n"
+        return string
 
+    def check_id(self, id_num, id_name="id"):
+        """will return true if id is NOT in use
+        """
+        temp_ids = []
+        for key, value in self.tables_dict.items():
+            for item in value:
+                temp_ids.append(item[id_name])
+        if id_num not in temp_ids:
+            return True
+        else:
+            return False
 
+    def auto_id(self):
+        """return usable, unique id for tables_dict
+        """
+        id_num = 0
+        while not self.check_id(id_num):
+            id_num += 1
+        return id_num
 
+    def delete_entry(self, table, id_num):
+        """will mark entry for delete
+        """
+        pass
 
+    def add_column(self, table, column_name, data_type):
+        """It will mark, that you want add new column into table.
+        To avoid errors use this method separately from other methods and save.
+        data_type integer, string/text, none/null, real/float, blob.
+        """
+        pass
+
+    def update_table(self, table, dictionary):
+        """
+        dict need to be dict with id key and column_name key that you want edit
+        """
--- timer.py
+++ timer.py
@@ -1,144 +1,149 @@
-from sql.sql_io import Database
+# from sql.sql_io import Database
 import datetime
 
 
+class Timer:
+    """class with purpose to use for measure time
+    """
 
-class Timer():
-	"""class with purpose to use for measure time
-	"""
-	def __init__(self):
-		self.data = {"timer":[]}
-		self.time = []
-		self.comments = ""
-		self.tags = {"python":0, "sql":0, "eng":0, "jap":0, "other":0}
-		self.id = 0
-		
-	
-	
-	def start(self):
-		"""start time
-		"""
-		self.time = []
-		self.time.append(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-		print("The program starts counting the time.")
-		
-		
-	def end(self):
-		"""end time
-		"""
-		if self.time:
-			self.time.append(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-			print("The program ends the time counting.")
-		else:
-			print("First use command start.")
-	
-	
-	def comment(self):
-		"""take comment
-		"""
-		self.comments = ""
-		self.comments+=" "+input("Please write comment: ")
-	
-			
-	def reset_tags(self):
-		for key, value in self.tags.items():
-			self.tags[key] = 0
-		
-	def add_tags(self):
-		"""Take input whih tags marks
-		"""
-		t = ""
-		for item in self.tags.keys():
-			t+=item+" "
-		text = "Please write tags you want separate by space. Tags you can choose are: "+t+"\n"
-		inp = input(text)
-		inp = inp.split(" ")
-		while True:
-			index = 0	
-			for item in inp:
-				if item in self.tags:
-					index+=1
-				else:
-					print("There is no such tag: "+item)
-					inp = input(text)
-					inp = inp.split(" ")
-					break
-			if index == len(inp):
-				break
-		for item in inp:
-			self.tags[item] = 1
-				
+    def __init__(self):
+        self.data = {"timer": []}
+        self.time = []
+        self.comments = ""
+        self.tags = {"python": 0, "sql": 0, "eng": 0, "jap": 0, "other": 0}
+        self.id = 0
 
-	def reset_data(self):
-		"""reset self.data
-		"""
-		self.data = {"timer":[]}
+    def start(self):
+        """start time
+        """
+        self.time = []
+        self.time.append(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        print("The program starts counting the time.")
 
+    def end(self):
+        """end time
+        """
+        if self.time:
+            self.time.append(datetime.datetime.now().strftime
+                             ("%Y-%m-%d %H:%M:%S"))
+            print("The program ends the time counting.")
+        else:
+            print("First use command start.")
 
+    def comment(self):
+        """take comment
+        """
+        self.comments = ""
+        self.comments += " " + input("Please write comment: ")
 
+    def reset_tags(self):
+        for key, value in self.tags.items():
+            self.tags[key] = 0
 
-	def set_data(self):
-		"""set all need data into usefull schema
-		"""
-		if self.time:
-			combine = {"id":self.id, "start":self.time[0], "end":self.time[1], "comment":self.comments, **self.tags}
-			self.data["timer"].append(combine)
-		else:
-			print("Please start time count.")
-		
-	def set_id(self):
-		"""alow set id number
-		"""
-		self.id = input("Please write id number for this session: ")
-	
+    def add_tags(self):
+        """Take input with tags marks
+        """
+        t = ""
+        for item in self.tags.keys():
+            t += item + " "
+        text = "Please write tags you want separate by space. Tags you can "\
+               "choose are: " + t + "\n "
+        inp = input(text)
+        inp = inp.split(" ")
+        while True:
+            index = 0
+            for item in inp:
+                if item in self.tags:
+                    index += 1
+                else:
+                    print("There is no such tag: " + item)
+                    inp = input(text)
+                    inp = inp.split(" ")
+                    break
+            if index == len(inp):
+                break
+        for item in inp:
+            self.tags[item] = 1
+
+    def reset_data(self):
+        """reset self.data
+        """
+        self.data = {"timer": []}
+
+    def set_data(self):
+        """set all need data into useful schema
+        """
+        if self.time:
+            combine = {"id": self.id, "start": self.time[0],
+                       "end": self.time[1], "comment": self.comments,
+                       **self.tags}
+            self.data["timer"].append(combine)
+        else:
+            print("Please start time count.")
+
+    def set_id(self):
+        """allow set id number
+        """
+        self.id = input("Please write id number for this session: ")
 
 
-	
 def initialize(t, d):
-	"""start program data
-	"""
-	text = "Please write command, if need help, write \"help\" for list of commands.\n"
-	help = "Commands are: \n\"auto\" for start program automatically.\nCommands for manual usage:\n\"start_time\" for starting time count.\n\"end_time\" for end time count.\n\"comment\" for add coment.\n\"tag\" for add tags.\n\"id\" for chose id number.\n\"read\" for see session entry\n\"save\" for saving session\n\n\"close\" for closing program."
-	while True:
-		inp = input(text)
-		
-		if inp=="auto":
-			t.start()
-			while True:
-				input("Press ENTER to ends the time counting.")
-				q = input("Are you sure that you want to ends the time counting?\nWrite \"y\" if yes. ")
-				if q=="y" or q=="Y":
-					break
-			t.end()
-			t.comment()
-			t.add_tags()
-			t.set_data()
-			d.add_entry(t.data)
-			t.reset_data()
-			t.reset_tags()
-			
-		elif inp=="start_time":
-			t.start()
-		elif inp=="end_time":
-			t.end()
-		elif inp=="comment":
-			t.comment()
-		elif inp=="tag":
-			t.add_tags()
-		elif inp=="id":
-			t.set_id()
-			if not d.check_id(t.id):
-				print("This id: {} is in use, if you don't set other id, id will change automatically when save.".format(t.id))
-			else:
-				print("Id was set to {}.".format(t.id))
-		elif inp=="read":
-			print("Database: ", d.read())
-		elif inp=="save":
-			d.add_entry(t.data)
-			t.reset_data()
-		elif inp=="close":
-				break
-		elif inp=="help":
-			print(help)
-		else:
-			print("I don't recognise this command, for commands list use \"help\"")
+    """start program data
+    """
+    text = "Please write command, if need help, write \"help\" for list of "\
+           "commands.\n "
+    help_txt = "Commands are: \n\"auto\" for start program automatically."\
+               "\nCommands for manual usage:\n\"start_time\" for "\
+               "starting time count.\n\"end_time\" for end time "\
+               "count.\n\"comment\" for add comment.\n\"tag\" for add "\
+               "tags.\n\"id\" for chose id number.\n\"read\" for see session "\
+               "entry\n\"save\" for saving session\n\n\"close\" for closing "\
+               "program. "
+    while True:
+        inp = input(text)
+
+        if inp == "auto":
+            t.start()
+            while True:
+                input("Press ENTER to ends the time counting.")
+                q = input("Are you sure that you want to ends the time "
+                          "counting?\nWrite \"y\" if yes. ")
+                if q == "y" or q == "Y":
+                    break
+            t.end()
+            t.comment()
+            t.add_tags()
+            t.set_data()
+            d.add_entry(t.data)
+            t.reset_data()
+            t.reset_tags()
+
+        elif inp == "start_time":
+            t.start()
+        elif inp == "end_time":
+            t.end()
+        elif inp == "comment":
+            t.comment()
+        elif inp == "tag":
+            t.add_tags()
+        elif inp == "id":
+            t.set_id()
+            if not d.check_id(t.id):
+                print(
+                    "This id: {} is in use, if you don't set other id, "
+                    "id will change automatically when save.".format(
+                        t.id))
+            else:
+                print("Id was set to {}.".format(t.id))
+        elif inp == "read":
+            print("Database: ", d.read())
+        elif inp == "save":
+            d.add_entry(t.data)
+            t.reset_data()
+        elif inp == "close":
+            break
+        elif inp == "help":
+            print(help_txt)
+        else:
+            print("I don't recognise this command, for commands list use"  
+                  "\"help\"")
